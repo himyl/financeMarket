d3f74bda7f20023c3ed1cae231ca960f9cf71510
Mengyao Li <himengyaoli@gmail.com>
2024-11-12 08:44:43 +0800

    Update getResScp_hdp.sh

--- src/main/shell/getResScp_hdp.sh
+++ src/main/shell/getResScp_hdp.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-####product="creditcard或loan" scene="will或connect" 模型入表319
 product=$1
 scene=$2
 proc_date=$3
 
-####变量
+#### various
 hdfs_path="/tmp/finance/all_user/"${product}_${scene}_${proc_date}
 local_path="/data01/ubd_obx_test/tx/projects_data/all_user/"${product}_${scene}_${proc_date}
 target_path="/data/ubd_ana_test/tx/projects_data/all_user/"${product}_${scene}_${proc_date}
 
 ip_319='10.191.21.11'
 
-####预测结果scp
+#### res scp
 hadoop fs -get ${hdfs_path} ${local_path}
 echo "get succeed"
 
 scp -r ${local_path} ubd_ana_test@${ip_319}:${target_path}
 echo "scp succeed"
 
-####登陆319
 to_run="/data/ubd_ana_test/tx/chinaunicom_finance_marketing_distributed_adv/src/main/shell/loadInTable.sql"
 ssh ubd_ana_test@${ip_319} << EOF
 source /opt/beh/conf/beh_env;
