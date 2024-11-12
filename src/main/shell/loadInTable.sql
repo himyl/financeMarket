b611da3fd0264dffa92f4b8669487bfe6670d35f
Mengyao Li <himengyaoli@gmail.com>
2024-11-12 09:02:38 +0800

    Update loadInTable.sql

--- src/main/shell/loadInTable.sql
+++ src/main/shell/loadInTable.sql
@@ -1,10 +1,6 @@
---loan
---419队列名称
---set mapreduce.job.queuename=ia_ana;
---319队列
-set mapreduce.job.queuename=xl_non_production;
---设置任务名
-set mapred.job.name=udb-jsb-tx;
+
+set mapreduce.job.queuename=XXXXX;
+set mapred.job.name=XXXXX;
 
 set hive.merge.mapredfiles=true;
 set hive.merge.smallfiles.avgsize=134217728;
@@ -17,6 +13,6 @@ set hive.hadoop.supports.splittable.combineinputformat=true;
 set hive.cli.print.header=true;
 set hive.resultset.use.unique.column.names=false;
 
---入表
+--load table
 load data local inpath '${hivevar:data_path}'
 into table ubd_ana_test_2.dwa_d_finance_cust_marketing_${hivevar:product}_${hivevar:scene} partition(date_id=${hivevar:proc_date});
