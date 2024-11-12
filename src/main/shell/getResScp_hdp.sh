83e18f72de6dffadb0adfe7c9411892d9663377f
Mengyao Li <himengyaoli@gmail.com>
2024-11-12 09:01:42 +0800

    Update getResScp_hdp.sh

--- src/main/shell/getResScp_hdp.sh
+++ src/main/shell/getResScp_hdp.sh
@@ -5,21 +5,21 @@ scene=$2
 proc_date=$3
 
 #### various
-hdfs_path="/tmp/finance/all_user/"${product}_${scene}_${proc_date}
-local_path="/data01/ubd_obx_test/tx/projects_data/all_user/"${product}_${scene}_${proc_date}
-target_path="/data/ubd_ana_test/tx/projects_data/all_user/"${product}_${scene}_${proc_date}
+hdfs_path=""
+local_path=""
+target_path=""
 
-ip_319='10.191.21.11'
+ip_1=""
 
 #### res scp
 hadoop fs -get ${hdfs_path} ${local_path}
 echo "get succeed"
 
-scp -r ${local_path} ubd_ana_test@${ip_319}:${target_path}
+scp -r ${local_path} ubd_ana_test@${ip_1}:${target_path}
 echo "scp succeed"
 
-to_run="/data/ubd_ana_test/tx/chinaunicom_finance_marketing_distributed_adv/src/main/shell/loadInTable.sql"
-ssh ubd_ana_test@${ip_319} << EOF
+to_run="XXX/loadInTable.sql"
+ssh XXX@${ip_1} << EOF
 source /opt/beh/conf/beh_env;
 hive --hivevar data_path=${target_path} \
      --hivevar product=${product} \
